2e253b0a1b621289e3330fac4a707875e3e67b44
Jean-Romain Roy <jroy@charitableanalytics.org>
2022-01-13 14:02:35 +0200

    loader when slides are being loaded

--- src/article/Slides.svelte
+++ src/article/Slides.svelte
@@ -6,13 +6,23 @@
     // load constants
     import { SUBDIRECTORY } from '../constants.json';
 
+    // import components
+    import Loader from '../components/Loader.svelte';
+
+    // keep track of the images that are loaded
+    let loaded_images = {}
+    indexes.forEach(index => { loaded_images[index] = false; })
+
 </script>
 
 <!-- Slides -->
 {#each indexes as index}
     <section>
         <div class="slide-container">
-            <img alt="slide #{index}" src="{SUBDIRECTORY}assets/slides/{index}.svg"/>
+            {#if !loaded_images[index]}
+                <Loader/>
+            {/if}
+            <img on:load={() => { loaded_images[index] = true; }} alt="slide #{index}" src="{SUBDIRECTORY}assets/slides/{index}.svg"/>
         </div>
     </section>
 {/each}
